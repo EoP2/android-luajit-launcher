ce812c46bdc809ad0adbdcbf568d2cb484eca6f1
EoP2 <41232498+EoP2@users.noreply.github.com>
2026-03-10 00:27:09 +0800

    Update MainActivity.kt

--- app/src/main/java/org/koreader/launcher/MainActivity.kt
+++ app/src/main/java/org/koreader/launcher/MainActivity.kt
@@ -819,16 +819,7 @@ class MainActivity : NativeActivity(), LuaInterface,
     @Suppress("DEPRECATION")
     private fun setFullscreenLayout() {
         val decorView = window.decorView
-        when {
-            Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT ->
-                decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN or
-                    View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or
-                    View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY or
-                    View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
-                    View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
-                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-            else -> decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LOW_PROFILE
-        }
+        decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_STABLE
     }
 
     private class Box<T> {
